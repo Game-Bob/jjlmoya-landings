4dc16bdf508035593d486de51d5df31346335ef3
jjlmoya <juanjo1989@gmail.com>
2026-07-17 00:56:07 +0200

    chore: testing

---
+++ src/data.ts
@@ -0,0 +1,6 @@
+import { teamEntry } from './landing/team/entry';
+
+export const ALL_LANDING_ENTRIES = [teamEntry];
+
+export { teamEntry };
+export type { LandingCardContent, LandingEntry, KnownLocale } from './types';
---
+++ src/landing/team/runtime.ts
@@ -0,0 +1,7 @@
+import { teamEntry } from './entry';
+import LandingComponent from './landing.astro';
+
+export const TEAM_LANDING = {
+    entry: teamEntry,
+    LandingComponent,
+};
