af3485c8ece345a9bc75c9b6ef1ae0ec60eac83f
jjlmoya <juanjo1989@gmail.com>
2026-07-17 01:04:22 +0200

    chore: testing

--- src/data.ts
+++
@@ -1,6 +0,0 @@
-import { teamEntry } from './landing/team/entry';
-
-export const ALL_LANDING_ENTRIES = [teamEntry];
-
-export { teamEntry };
-export type { LandingCardContent, LandingEntry, KnownLocale } from './types';
--- src/landing/team/runtime.ts
+++
@@ -1,7 +0,0 @@
-import { teamEntry } from './entry';
-import LandingComponent from './landing.astro';
-
-export const TEAM_LANDING = {
-    entry: teamEntry,
-    LandingComponent,
-};
